a1408205dbcb8df95d0df31c60e21faee73381bb
Josh Boudreau <joshuaboud@hotmail.com>
2022-06-29 00:34:13 -0300

    filter relative entities before running system

--- src/ECS/ECS.ts
+++ src/ECS/ECS.ts
@@ -26,15 +26,15 @@ class ECS {
 		const systemsCount = this.systems.length;
 		for (let i = 0; i < systemsCount; i++) {
 			const system = this.systems[i];
-			// const relevantComponents = this.systems[i].relevantComponents ?? [];
-			// const relevantEntities = system.relevantComponents
-			// 	? this.entities
-			// 		.filter(entity =>
-			// 			relevantComponents
-			// 				.some(component =>
-			// 					component in entity.components))
-			// 	: this.entities;
-			system(this.entities, delta);
+			const relevantComponents = this.systems[i].relevantComponents ?? [];
+			const relevantEntities = system.relevantComponents
+				? this.entities
+					.filter(entity =>
+						relevantComponents
+							.some(component =>
+								component in entity.components))
+				: this.entities;
+			system(relevantEntities, delta);
 		}
 
 		if (this.running !== false) {
